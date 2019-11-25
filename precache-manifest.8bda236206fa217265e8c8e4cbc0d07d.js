3f8f6e005cfae8f222c967fdf720ed50a5105c84
root <lava.mahmoud89@gmail.com>
2019-11-25 10:29:24 +0100

    Updates

---
+++ precache-manifest.8bda236206fa217265e8c8e4cbc0d07d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6705d511f66f748dde04504c69dc190f",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "9476d231409724236ca0",
+    "url": "/todolist/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "826258050bf4be3947f6",
+    "url": "/todolist/static/js/2.6a9c457a.chunk.js"
+  },
+  {
+    "revision": "9476d231409724236ca0",
+    "url": "/todolist/static/js/main.2193c097.chunk.js"
+  },
+  {
+    "revision": "2265ce099de690ea8e4b",
+    "url": "/todolist/static/js/runtime-main.c711f2e0.js"
+  }
+]);
